0bbc1de8c5ed52b5fee1146f2683a90ed9ae7ac4
Aswin-PK <aswinpk7737@gmail.com>
2023-11-13 15:24:56 +0530

    static load

--- KJC_Nest/urls.py
+++ KJC_Nest/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Superadmin.urls')),
     path('hostel/', include('Hostel.urls')),
     path('guesthouse/', include('GuestHouse.urls')),
 ]
