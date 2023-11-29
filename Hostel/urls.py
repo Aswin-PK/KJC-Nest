b05e24c61f0aff3be0f3e31ee47a827be9afa019
Johnpaulreju <johnpaulreju2k@gmail.com>
2023-11-29 13:00:58 +0530

    student add

--- Hostel/urls.py
+++ Hostel/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<str:user>/transactions/', views.transactions, name="transactions"),
     path('<str:user>/fee_payment/', views.fee_payment, name="fee_payment"),
     path("<str:user>/hroomsave/", views.hroomsave, name="hroomsave"),
+    path('<str:user>/add_student/', views.add_student, name='add_student'),
 ]
